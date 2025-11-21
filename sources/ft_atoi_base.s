9f52d2aad74268aad444eb590b6f9d1ce05d4e56
MykleR <michael.rouves@gmail.com>
2025-11-21 13:19:07 +0100

    🚧wip: improved a lot ft_check_base for ft_atoi_base

--- sources/ft_atoi_base.s
+++ sources/ft_atoi_base.s
@@ -2,55 +2,31 @@ global ft_atoi_base
 
 section .text
 
-%macro ALLOC_ZERO_STACK 1
-    ; Allocates %1 bytes on the stack and zeros them with rep stosb.
-    ; Clobbers: rcx, al; caller must preserve if needed.
-    sub     rsp, %1
-    mov     rcx, %1
-	push	rdi
-    lea     rdi, [rsp]
-    xor     al, al
-    cld
-    rep     stosb
-	pop		rdi
-%endmacro
-
-%macro LOT_CHECK 2
-	cmp		byte [%1], 0
-	jnz		%2
-%endmacro
-
 ft_check_base:
 .init_array:
-	ALLOC_ZERO_STACK 256	; Allocate 256 bytes on stack and zero them
-   	mov		rax, rsi		; Copy pointer to char *base
-   	jmp		.loop_cond
-.loop_body:
-   	mov		byte [rsp + rcx], 1
-    inc		rax
+    sub     rsp, 16
+	mov		rax, 0x280100003e00		; Binary mask sets forbidden chars to 1 in lookup tab
+	mov		qword [rsp], rax		; Init first half with mask
+	mov		qword [rsp + 8], 0		; Init second half with zero
+	xor		rdx, rdx
+   	mov		rax, rsi				; Copy pointer to char *base
 .loop_cond:
-	movzx	rcx, byte [rax]
-	cmp		byte [rsp + rcx], 0
-	jnz		.fail
-	test	cl, cl
-	jnz		.loop_body
+	movzx	r8, byte [rax]
+    mov		r9, r8					; r9 = C
+	test	r8b, r8b
+	jz		.loop_end
+    shr     r8, 3					; r8 = byte index
+    and     r9, 7					; r9 = bit index
+    bts     [rsp + r8], r9			; Test and set the bit
+    jc      .fail                   ; If carry flag is set, it was a duplicate
+    inc		rax
+	jmp		.loop_cond
 .loop_end:
 	sub		rax, rsi
 	cmp		rax, 2
-	jl		.fail
-	LOT_CHECK rsp + '+', .fail
-	LOT_CHECK rsp + '-', .fail
-	LOT_CHECK rsp + ' ', .fail
-	LOT_CHECK rsp + 9, .fail	;\r
-	LOT_CHECK rsp + 10, .fail	;\n
-	LOT_CHECK rsp + 11, .fail	;\t
-	LOT_CHECK rsp + 12, .fail	;\v
-	LOT_CHECK rsp + 13, .fail	;\f
-	add		rsp, 256	; Clear Stack
-	ret
 .fail:
-	add		rsp, 256	; Clear Stack
-	xor		rax, rax
+	cmovc	rax, rdx		; mov 0 in rax if less than 2
+	add		rsp, 16
 	ret
 
 ft_atoi_base:
